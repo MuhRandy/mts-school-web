abd4575a76903b994a1e1c551a23ae27aa86496e
MuhRandy <muhammadrandy0027@gmail.com>
2023-11-22 08:51:40 +0700

    set up articles state to App component instead

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,46 @@ import Profil from "./pages/Profil";
 import Footer from "./components/Footer";
 import NewsAndArticles from "./pages/NewsAndArticles";
 import Login from "./components/Login";
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import CreatePost from "./components/CreatePost";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./utils/firebase";
 
 type GlobalContent = {
   isAuth: boolean;
   setIsAuth: (isAuth: boolean) => void;
+  articles: any;
   navigate: NavigateFunction;
 };
 
 export const AppContext = createContext<GlobalContent>({
   isAuth: false,
   setIsAuth: () => {},
+  articles: [],
   navigate: () => {},
 });
 
 export const useAppContext = () => useContext(AppContext);
 
 function App() {
+  const [articles, setArticles] = useState<any[]>([]);
   const [isAuth, setIsAuth] = useState(Boolean(localStorage.getItem("isAuth")));
   const navigate = useNavigate();
 
+  const postCollectionRef = collection(db, "posts");
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postCollectionRef);
+      setArticles(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getPosts();
+  }, []);
+
   return (
     <ChakraProvider>
-      <AppContext.Provider value={{ isAuth, setIsAuth, navigate }}>
+      <AppContext.Provider value={{ isAuth, setIsAuth, articles, navigate }}>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
--- src/components/NewsAndArticles/Articles.tsx
+++ src/components/NewsAndArticles/Articles.tsx
@@ -1,40 +1,15 @@
 import { Wrap, WrapItem } from "@chakra-ui/react";
 import Content from "../Content";
 import Article from "../Article";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../utils/firebase";
-import { useEffect, useState } from "react";
+import { useAppContext } from "../../App";
 
 const Articles = () => {
-  const postCollectionRef = collection(db, "posts");
-
-  // type articleProps = {
-  //   author: {
-  //     id: string;
-  //     name: string;
-  //   };
-  //   id: string;
-  //   title: string;
-  //   post: string;
-  // };
-
-  const [articles, setArticles] = useState<any[] | null[]>([]);
-
-  useEffect(() => {
-    const getPosts = async () => {
-      const data = await getDocs(postCollectionRef);
-      setArticles(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-
-    getPosts();
-  }, []);
-
-  console.log(articles);
+  const { articles } = useAppContext();
 
   return (
     <Content title="Artikel">
       <Wrap justify={"center"}>
-        {articles?.map((article) => (
+        {articles?.map((article: any) => (
           <WrapItem key={article.id}>
             <Article title={article.title} postText={article.post} />
           </WrapItem>
