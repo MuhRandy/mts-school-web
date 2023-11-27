9e30ef8cdb900968e15f6c337875c3bf49b5e318
MuhRandy <muhammadrandy0027@gmail.com>
2023-11-27 14:40:38 +0700

    update login to just one account

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,22 @@
-import { auth, provider } from "../utils/firebase";
-import { signInWithPopup } from "firebase/auth";
-import { Button, Center } from "@chakra-ui/react";
+import { auth } from "../utils/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  Button,
+  Center,
+  FormControl,
+  FormLabel,
+  HStack,
+  Input,
+  VStack,
+} from "@chakra-ui/react";
 import { useAppContext } from "../App";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Login = () => {
   const { isAuth, setIsAuth, navigate } = useAppContext();
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
   useEffect(() => {
     if (isAuth) {
@@ -13,19 +24,56 @@ const Login = () => {
     }
   }, []);
 
-  const signInWithGoogle = () => {
-    signInWithPopup(auth, provider).then(() => {
-      localStorage.setItem("isAuth", "true");
-      setIsAuth(true);
-      navigate("/");
-    });
+  // const signInWithGoogle = () => {
+  //   signInWithPopup(auth, provider).then(() => {
+  //     localStorage.setItem("isAuth", "true");
+  //     setIsAuth(true);
+  //     navigate("/");
+  //   });
+  // };
+
+  const signIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        localStorage.setItem("isAuth", "true");
+        setIsAuth(true);
+        navigate("/");
+      })
+      .catch((err) => {
+        alert("Email atau Password yang Anda Masukkan Salah!");
+        console.log(err);
+      });
   };
 
   return (
     <Center className="min-h-screen">
-      <Button onClick={signInWithGoogle} colorScheme="blackAlpha">
+      <VStack gap={3}>
+        <FormControl>
+          <HStack>
+            <FormLabel whiteSpace={"nowrap"}>Email :</FormLabel>
+            <Input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </HStack>
+        </FormControl>
+        <FormControl>
+          <HStack>
+            <FormLabel whiteSpace={"nowrap"}>Password :</FormLabel>
+            <Input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </HStack>
+        </FormControl>
+        <Button onClick={signIn}>Login</Button>
+      </VStack>
+      {/* <Button onClick={signInWithGoogle} colorScheme="blackAlpha">
         Login With Google
-      </Button>
+      </Button> */}
     </Center>
   );
 };
