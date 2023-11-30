229d3a2a6433b91b14ccd357100708b304d1be3a
MuhRandy <muhammadrandy0027@gmail.com>
2023-11-30 15:04:52 +0700

    update data from database instead, not from App so not error when refresh

--- src/pages/SinglePost.tsx
+++ src/pages/SinglePost.tsx
@@ -1,78 +1,102 @@
-import { IconCalendarStats, IconUserCircle } from '@tabler/icons-react';
-import { Container } from '@chakra-ui/react';
-import { useAppContext } from '../App';
+import { IconCalendarStats, IconUserCircle } from "@tabler/icons-react";
+import { Container } from "@chakra-ui/react";
+import { useAppContext } from "../App";
+import { DocumentData, doc, getDoc } from "firebase/firestore";
+import { db } from "../utils/firebase";
+import { useEffect, useState } from "react";
+import LoadingSection from "../components/LoadingSection";
 
 function SinglePost() {
   // get state from App
-  const { news } = useAppContext();
+  const { isLoading, setIsLoading } = useAppContext();
+
+  const [post, setPost] = useState<DocumentData>();
 
   // get post id from search params and store it at postID
   const searchParams = new URLSearchParams(location.search);
-  const postID = searchParams.get('id');
+  const postID: string = searchParams.get("id")!;
 
-  // filter post based on id which is unique so just one post will be returned and store it at singlePost
-  const filteredPost = news.filter((post: any) => {
-    return post.id == postID;
-  });
-  const singlePost = filteredPost[0];
+  // get post from firestore based on postID
+  const getPost = async () => {
+    setIsLoading(true);
+    const docRef = doc(db, "news", postID);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data().title);
+      setPost(docSnap.data());
+      setIsLoading(false);
+    } else {
+      // docSnap.data() will be undefined in this case
+      console.log("No such document!");
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getPost();
+  }, []);
 
   // get post date from database
-  const date = singlePost.timestamp.toDate();
-  const days = ['Ming', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'];
+  const date = post?.timestamp.toDate();
+  const days = ["Ming", "Sen", "Sel", "Rab", "Kam", "Jum", "Sab"];
   const months = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'Mei',
-    'Jun',
-    'Jul',
-    'Ags',
-    'Sep',
-    'Okt',
-    'Nov',
-    'Des',
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "Mei",
+    "Jun",
+    "Jul",
+    "Ags",
+    "Sep",
+    "Okt",
+    "Nov",
+    "Des",
   ];
-  const day = days[date.getDay()];
-  const month = months[date.getMonth()];
+  const day = days[date?.getDay()];
+  const month = months[date?.getMonth()];
 
   return (
-    <Container maxW={{ md: '70vw' }}>
-      <div className="p-2">
-        {singlePost?.imgUrl && (
-          <img
-            src={singlePost.imgUrl}
-            alt={`Image Header for ${singlePost.title}`}
-            className="w-full object-cover h-[400px]"
-          />
-        )}
-        <h1 className="text-4xl font-bold my-3 text-center">
-          {singlePost?.title}
-        </h1>
-        {/* Author & Timestamp */}
-        <div className="py-5 mb-7 flex flex-col items-center justify-between border-b-2 border-black sm:flex-row">
-          {/* Author */}
-          <div className="flex gap-2 items-center">
-            <IconUserCircle
-              size={35}
-              className="hover:scale-125 transition duration-500"
+    <Container maxW={{ md: "70vw" }}>
+      {!isLoading ? (
+        <div className="p-2">
+          {post?.imgUrl && (
+            <img
+              src={post?.imgUrl}
+              alt={`Image Header for ${post?.title}`}
+              className="w-full object-cover h-[400px]"
             />
-            <span className="font-semibold">@admin</span>
+          )}
+          <h1 className="text-4xl font-bold my-3 text-center">{post?.title}</h1>
+          {/* Author & Timestamp */}
+          <div className="py-5 mb-7 flex flex-col items-center justify-between border-b-2 border-black sm:flex-row">
+            {/* Author */}
+            <div className="flex gap-2 items-center">
+              <IconUserCircle
+                size={35}
+                className="hover:scale-125 transition duration-500"
+              />
+              <span className="font-semibold">@admin</span>
+            </div>
+            {/* Timestamp */}
+            <div className="flex items-center gap-1">
+              <IconCalendarStats size={20} stroke={1} />
+              <span className="font-semibold">
+                {day}, {date?.getDate()} {month} {date?.getFullYear()}
+              </span>
+            </div>
           </div>
-          {/* Timestamp */}
-          <div className="flex items-center gap-1">
-            <IconCalendarStats size={20} stroke={1} />
-            <span className="font-semibold">
-              {day}, {date.getDate()} {month} {date.getFullYear()}
-            </span>
-          </div>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: post?.post!,
+            }}
+            className="ql-editor"
+          />
         </div>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: singlePost?.post!,
-          }}
-        ></div>
-      </div>
+      ) : (
+        <LoadingSection />
+      )}
     </Container>
   );
 }
