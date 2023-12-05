2221bdddbefd60d12827cc01febac6bb1ace3de0
unknown <muhammadrandy0027@gmail.com>
2023-12-05 12:59:40 +0800

    update theme color

--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -9,15 +9,15 @@ import {
   IconButton,
   Show,
   useDisclosure,
-} from "@chakra-ui/react";
-import { HamburgerIcon } from "@chakra-ui/icons";
-import { useNavigate } from "react-router-dom";
-import { useAppContext } from "../../App";
-import { signOut } from "firebase/auth";
-import { auth } from "../../utils/firebase";
-import { useEffect, useState } from "react";
-import clsx from "clsx";
-import { cn } from "../../utils/utils";
+} from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
+import { useNavigate } from 'react-router-dom';
+import { useAppContext } from '../../App';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../utils/firebase';
+import { useEffect, useState } from 'react';
+import clsx from 'clsx';
+import { cn } from '../../utils/utils';
 
 function Navbar() {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -31,7 +31,7 @@ function Navbar() {
     signOut(auth).then(() => {
       localStorage.clear();
       setIsAuth(false);
-      navigate("/login");
+      navigate('/login');
     });
   };
 
@@ -49,27 +49,27 @@ function Navbar() {
     <>
       <nav
         className={cn(
-          "bg-[#99fc08] mx-5 rounded-md",
-          "sticky top-0 transition-all duration-300",
-          ["min-[768px]:p-3"],
+          'bg-[#009f5b] mx-5 rounded-md',
+          'sticky top-0 transition-all duration-300',
+          ['min-[768px]:p-3'],
           {
-            "fixed top-0 left-0 right-0 z-50 mx-0 rounded-none": isSticky,
+            'fixed top-0 left-0 right-0 z-50 mx-0 rounded-none': isSticky,
           }
         )}
       >
         <Hide above="md">
           <IconButton
             aria-label="Menu"
-            icon={<HamburgerIcon color={"white"} fontSize={{ sm: "xl" }} />}
+            icon={<HamburgerIcon color={'white'} fontSize={{ sm: 'xl' }} />}
             onClick={onOpen}
-            bg={"#99fc08"}
+            bg={'#009f5b'}
           />
         </Hide>
         <Show above="md">
           <ul
-            className={clsx("flex gap-2 text-white", [
-              "min-[768px]:text-lg",
-              "min-[992px]:text-2xl",
+            className={clsx('flex gap-2 text-white', [
+              'min-[768px]:text-lg',
+              'min-[992px]:text-2xl',
             ])}
           >
             <li>
@@ -102,7 +102,7 @@ function Navbar() {
           </ul>
         </Show>
       </nav>
-      <Drawer placement={"left"} onClose={onClose} isOpen={isOpen} size={"xs"}>
+      <Drawer placement={'left'} onClose={onClose} isOpen={isOpen} size={'xs'}>
         <DrawerOverlay />
         <DrawerContent>
           <DrawerCloseButton />
